1a0fd88b0bcb512e7fbafc1dc69dc84954343a50
Vlad_B <vbh@localhost.localdomain>
2019-03-30 11:47:46 +0100

    wh

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ WEBHOOK_HOST = '109.87.105.97'
 WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
 WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
 
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
+WEBHOOK_SSL_CERT = './ssl/webhook_cert.pem'  # Путь к сертификату
+WEBHOOK_SSL_PRIV = './ssl/webhook_pkey.pem'  # Путь к приватному ключу
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % (token)
